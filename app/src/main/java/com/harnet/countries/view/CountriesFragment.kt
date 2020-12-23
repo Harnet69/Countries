233fef8321c9b183db143116409e154f806974c2
Harnet69 <harnet69@gmail.com>
2020-12-23 10:08:56 +0100

    Implemented countries viewModel

--- app/src/main/java/com/harnet/countries/view/CountriesFragment.kt
+++ app/src/main/java/com/harnet/countries/view/CountriesFragment.kt
@@ -2,18 +2,16 @@ package com.harnet.countries.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.harnet.countries.viewModel.CountriesViewModel
 import com.harnet.countries.R
 
 class CountriesFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = CountriesFragment()
-    }
 
     private lateinit var viewModel: CountriesViewModel
 
@@ -22,10 +20,22 @@ class CountriesFragment : Fragment() {
         return inflater.inflate(R.layout.countries_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(CountriesViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.refresh()
+
+        observeModel()
+
+    }
+
+    private fun observeModel(){
+
+        viewModel.getMCountries().observe(viewLifecycleOwner, Observer {
+            Log.i("countriesFromApi", "onViewCreated: $it")
+        })
+
     }
 
 }
--- app/src/main/java/com/harnet/countries/viewModel/CountriesViewModel.kt
+++ app/src/main/java/com/harnet/countries/viewModel/CountriesViewModel.kt
@@ -1,7 +1,32 @@
 package com.harnet.countries.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.harnet.countries.model.Country
 
 class CountriesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val mCountries = MutableLiveData<List<Country>>()
+
+    fun getMCountries() = mCountries
+
+    fun refresh(){
+        getCountryFromAPI()
+    }
+
+    private fun getCountryFromAPI(){
+        val countries = mutableListOf<Country>()
+    // mock countries getting
+        //TODO implement Retrofit functionality here
+        countries.add(Country("Russia", "Moscow", ""))
+        countries.add(Country("Poland", "Warsaw", ""))
+        countries.add(Country("Belarus", "Minsk", ""))
+        countries.add(Country("England", "London", ""))
+        countries.add(Country("USA", "Washington", ""))
+        countries.add(Country("Lithuania", "Vilnius", ""))
+
+        if(countries.isNotEmpty()){
+            mCountries.value = countries
+        }
+    }
 }
