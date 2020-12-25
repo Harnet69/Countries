9aef0908e089659b99703642358c59db2c511b43
Harnet69 <harnet69@gmail.com>
2020-12-25 08:57:23 +0100

    Implemented Error handler

--- app/src/main/java/com/harnet/countries/model/CountriesApiService.kt
+++ app/src/main/java/com/harnet/countries/model/CountriesApiService.kt
@@ -1,23 +1,20 @@
 package com.harnet.countries.model
 
-import androidx.annotation.Keep
-import io.reactivex.Single
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
 
 object CountriesApiService {
     // base URL of the API
     private val BASE_URL = "https://raw.githubusercontent.com"
+
     // object created by Retrofit for accessing to an endpoint
-    private val api =  Retrofit.Builder()
+    private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         // handle all basic communication, separate threads, errors and converts JSON to object of our class
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(CountriesApi::class.java)// create model class
-    
+
     //get observable List from API
     fun getCountries(): CountriesApi {
         return api
--- app/src/main/java/com/harnet/countries/viewModel/CountriesViewModel.kt
+++ app/src/main/java/com/harnet/countries/viewModel/CountriesViewModel.kt
@@ -4,17 +4,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.harnet.countries.model.CountriesApiService
 import com.harnet.countries.model.Country
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.*
 
 class CountriesViewModel : ViewModel() {
     private val countriesApiService = CountriesApiService.getCountries()
-    val job: Job? = null
-
-    private val disposable = CompositeDisposable()
+    var job: Job? = null
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        onError("Exception ${throwable.localizedMessage}")
+    }
 
     private val mCountries = MutableLiveData<List<Country>>()
     private val mIsLoading = MutableLiveData<Boolean>()
@@ -31,48 +28,35 @@ class CountriesViewModel : ViewModel() {
     private fun getCountryFromAPI() {
         mIsLoading.value = true
 
-        CoroutineScope(Dispatchers.IO).launch {
+        // variable job is used for cancelling if viewModel is desmissed
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            delay(1000L) // for simulating data downloading delaying
             val response = countriesApiService.getCountries()
             // switch for updating the UI thread
-            withContext(Dispatchers.Main){
-                if(response.isSuccessful){
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
                     response.body()?.let { retrieveCountries(it) }
-                }else{
-                    mErrorLoading.value = true
-                    mIsLoading.value = false
+                } else {
+                    onError("Exception ${response.message()}")
                 }
             }
         }
+    }
 
-
-
-//        disposable.add(
-//            // set it to a different thread(passing this call to the background thread)
-//            countriesApiService.getCountries()
-//                .subscribeOn(Schedulers.newThread())
-//                // retrieve it from a background to the main thread for displaying
-//                .observeOn(AndroidSchedulers.mainThread())
-//                // pass our Single object here, it's created and implemented
-//                .subscribeWith(object : DisposableSingleObserver<List<Country>>() {
-//                    // get list of DogBreed objects
-//                    override fun onSuccess(countriesList: List<Country>) {
-//                        retrieveCountries(countriesList)
-//                    }
-//
-//                    // get an error
-//                    override fun onError(e: Throwable) {
-//                        mErrorLoading.value = true
-//                        mIsLoading.value = false
-//                        // print stack of error to handling it
-//                        e.printStackTrace()
-//                    }
-//                })
-//        )
+    private fun onError(s: String) {
+        mErrorLoading.value = true
+        mIsLoading.value = false
     }
 
     private fun retrieveCountries(countriesList: List<Country>) {
         mCountries.value = countriesList
         mIsLoading.value = false
         mErrorLoading.value = false
+    }
+
+    // cancel a job when the viewModel will be terminated
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
     }
 }
